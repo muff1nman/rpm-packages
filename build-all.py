1284f5c223f64ef50d9d682a67c95b26a5d99a32
Andrew DeMaria <lostonamountain@gmail.com>
2015-11-11 00:28:56 -0500

    Output list of srpms

--- build-all.py
+++ build-all.py
@@ -3,6 +3,20 @@
 import ConfigParser
 import sys
 import subprocess
+import os
+
+from contextlib import contextmanager
+
+from glob import glob
+
+@contextmanager
+def cd(path):
+	prevdir = os.getcwd()
+	os.chdir(os.path.expanduser(path))
+	try:
+		yield
+	finally:
+		os.chdir(prevdir)
 
 # get bucket we are going to clear
 tito_config = ConfigParser.RawConfigParser()
@@ -26,3 +40,17 @@ else:
 index_upload_cmd = "s3cmd put index.html %s" %(s3bucket)
 print(index_upload_cmd)
 subprocess.check_call(index_upload_cmd, shell=True)
+
+paths = glob('*/')
+for path in paths:
+	print("Building specs in %s" %(path))
+	with cd(path):
+		tito_cmd = "tito release %s" %(sys.argv[1])
+		subprocess.check_call(tito_cmd, shell=True)
+
+# TODO use configured filetypes
+list_srpms = "s3cmd ls %s | awk '{print $4}' | sed 's/^s3/http/' | grep '\.src\.rpm$'" %(s3bucket)
+print(list_srpms)
+subprocess.check_call(list_srpms)
+
+# TODO send build to copr
